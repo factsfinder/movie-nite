86f3225260ab0b867d510e65ba115c84e7406913
factsfinder <phanindrasrikar@gmail.com>
2017-03-31 12:45:37 -0400

    Made a few configuration changes and trying to push into heroku

--- app/index.js
+++ app/index.js
@@ -1,6 +1,6 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var App = require('./components/app.js');
-require('./styles.css');
+var Styles = require('./styles.css');
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(App, document.getElementById('app'));
