97886ee83616dccb9ff5c13517c68a4facfc9532
factsfinder <phanindrasrikar@gmail.com>
2017-05-18 03:13:47 -0400

    fixed prod issues

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const express  = require('express');
 const path = require('path');
 const port = process.env.PORT || 8086;
 const app = express();
-app.use(express.static(__dirname + '/build/'));
+app.use(express.static(__dirname + '/dist/'));
 app.listen(port);
 console.log("server started");
